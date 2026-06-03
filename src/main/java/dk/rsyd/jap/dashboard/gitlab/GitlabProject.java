0e8f7b78472370578beff0aae8c4589a0964ee1a
CRye23 <130559601+CRye23@users.noreply.github.com>
2026-06-03 18:16:24 +0200

    Update GitlabProject.java

--- src/main/java/dk/rsyd/jap/dashboard/gitlab/GitlabProject.java
+++ src/main/java/dk/rsyd/jap/dashboard/gitlab/GitlabProject.java
@@ -2,6 +2,8 @@ package dk.rsyd.jap.dashboard.gitlab;
 
 import io.micronaut.serde.annotation.Serdeable;
 
+import static java.util.regex.Pattern.quote;
+
 @Serdeable
 public record GitlabProject(
     int id,
@@ -11,13 +13,19 @@ public record GitlabProject(
 
 
     public static GitlabProject fromJapNameSpace(GitlabClientDTOs.GitlabProject dto) {
-    String fullNamespace = dto.nameWithNamespace();
-    // Remove everything before and including " / jap / "
-    String afterJap = fullNamespace.replaceAll("(?i)^.*?\\s*/\\s*jap\\s*/\\s*", "");
-    // Remove the trailing /
-    String trailRemoved = afterJap.replaceAll("\\s*/\\s*$", "");
-    // Remove only the last occurrence of the project name - in case projectgroup shares the name
-    String projectGroup = trailRemoved.replaceFirst("\\s*/?\\s*" + java.util.regex.Pattern.quote(dto.name()) + "\\s*$", "");
-    return new GitlabProject(dto.id(), dto.name(), projectGroup, dto.gitlabUrl());
-}
+        String projectGroup = findProjectGroup(dto.nameWithNamespace(), dto.name());
+
+        return new GitlabProject(dto.id(), dto.name(), projectGroup, dto.gitlabUrl());
+    }
+
+    private static String findProjectGroup(String fullNamespace, String name) {
+        // Remove everything before and including " / jap / "
+        String afterJap = fullNamespace.replaceAll("(?i)^.*?\\s*/\\s*jap\\s*/\\s*", "");
+        // Remove the trailing /
+        String trailRemoved = afterJap.replaceAll("\\s*/\\s*$", "");
+        // Remove only the last occurrence of the project name - in case projectgroup shares the name
+
+        return trailRemoved.replaceFirst("\\s*/?\\s*" + quote(name) + "\\s*$", "");
+    }
+
 }
